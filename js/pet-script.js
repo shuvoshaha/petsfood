9184dec3d20d16e3f3bacfc61d0d4ba400e3a26f
shuvoshaha <shaha460@gmail.com>
2021-03-24 11:35:00 +0600

    updated

--- js/pet-script.js
+++ js/pet-script.js
@@ -6,3 +6,68 @@ const popUp =()=>{
 
 
 // Plugins Init
+
+// Swiper 
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+
+    
+    breakpoints: {
+        375: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            
+          },
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+          
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      },
+  
+
+    
+    autoplay: {
+        delay: 3000,
+      },
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+
+ // Owl Slider Init
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
